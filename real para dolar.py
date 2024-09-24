884d63c6fe279b7908f22e2e8a31fa67af774625
Ryanfrr <ryanfo132007@gmail.com>
2024-09-24 16:08:02 -0400

    Cotação do Dolar

---
+++ real para dolar.py
@@ -0,0 +1,7 @@
+valor = int(input("informe o valor  do produto em reais: "))
+valordolar = float(input("Qual o valor do dolar hoje?: "))
+cotacaodolar = valor/valordolar
+
+print ("O valor em reais é: ", valor)
+print (f"O Valor em dolar é:  {cotacaodolar :.2f}")
+
